d8e3abe33582be846b00811b015100f99d4746e9
murasanca <murasanca@protonmail.com>
2023-07-18 21:36:49 +0300

    All Mechanics and Example Level
    All mechanics are complete.
    All prefab needs are prepared.
    Car.cs script explained.
    Connection has been made with the new levels to be made.
    Fixed some UI needs.
    GameManager.cs script explained.
    Level designs can be made from the editor.

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -1,42 +1,83 @@
 using UnityEngine;
 
+// Car class that controls the movement and behavior of a car.
 public class Car:MonoBehaviour
 {
-    [Header("Transform Based on Relative Upward Direction"),SerializeField]
+    // Transform based on relative upward direction.
+    [Header("Transform Based on Relative Upward Direction"), SerializeField]
     private Transform relativeUp;
 
-    [Space(8),SerializeField]
-    private float speed=8f;
+    // Speed of the car.
+    [Space(8)]
+    [SerializeField]
+    private float speed = 32f;
+
+    // Reference to the GameManager.
+    private GameManager gameManager;
 
     /// <summary>
-    /// r2D: Rigidbody2D
+    /// r2D: Rigidbody2D component of the car.
     /// </summary>
     private Rigidbody2D r2D;
 
+    // Local position and rotation of the car.
+    private Vector3
+        localEulerAngles,
+        localPosition;
 
+
+    // Awake is called when the script instance is being loaded.
     private void Awake()
     {
+        // Find and store a reference to the GameManager.
+        gameManager=FindObjectOfType<GameManager>();
+
+        // Get and store a reference to the Rigidbody2D component.
         r2D=GetComponent<Rigidbody2D>();
+
+        // Store the initial local position and rotation of the car.
+        localEulerAngles=transform.localEulerAngles;
+        localPosition=transform.localPosition;
     }
 
-    private void Start()
-    {
-        
-    }
-
-    private void FixedUpdate()
-    {
-        r2D.MovePosition(r2D.position+(Vector2)(speed*Time.deltaTime*relativeUp.up));
-    }
+    // FixedUpdate is called every fixed frame-rate frame.
+    private void FixedUpdate() => r2D.MovePosition(r2D.position+(Vector2)(speed*Time.deltaTime*relativeUp.up));
 
 
+    // OnBecameInvisible is called when the renderer is no longer visible by any camera.
     private void OnBecameInvisible()
     {
-        // TODO: Restart.
+        // Restart the game and reset the car's transform.
+        gameManager.Restart();
+        ResetTransform();
     }
 
+    // OnCollisionEnter2D is called when this collider/rigidbody has begun touching another rigidbody/collider.
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // TODO: Restart.
+        if(collision.transform.CompareTag("Finish"))
+        {
+            // If the car collides with a finish line, disable its script, hide the finish line, and move on to the next car or level.
+            enabled=false;
+
+            collision.transform.parent.gameObject.SetActive(false);
+            gameManager.Next();
+        }
+        else // if(collision.transform.CompareTag("Obstacle"))
+        {
+            // If the car collides with an obstacle, restart the game and reset the car's transform.
+            gameManager.Restart();
+            ResetTransform();
+        }
+    }
+
+
+    /// <summary>
+    /// ResetTransform: Resets the car's local position and rotation to their initial values.
+    /// </summary>
+    public void ResetTransform()
+    {
+        transform.localEulerAngles=localEulerAngles;
+        transform.localPosition=localPosition;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,24 +1,175 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+// GameManager class that manages the game state and logic.
 public class GameManager:MonoBehaviour
 {
-    [Header("Cars in Starting Positions and Rotations"),SerializeField]
-    private GameObject[]cars;
+    // Rotation angle of buttons.
+    [Header("Rotation Angle of Buttons"), SerializeField]
+    private float angles = 22.5f;
 
+    // UI buttons.
+    [Header("Buttons"), SerializeField]
+    private GameObject playButton;
+    [SerializeField]
+    private GameObject
+        turnLeftButton,
+        turnRightButton;
+
+    // Cars in starting positions.
+    [Space(8)]
+    [Header("Cars in Starting Positions"), SerializeField]
+    private GameObject[] cars = new GameObject[8];
+
+    // Finish lines for each car.
+    [Header("Finish Lines"), SerializeField]
+    private GameObject[] finishes = new GameObject[8];
+
+    // Index of the current car being controlled by the player.
+    private int currentCarIndex = 0;
+
+    // Struct to store transform data (position and rotation).
+    private struct TransformData
+    {
+        public Quaternion rotation;
+        public Vector3 position;
+    }
+
+    // List of transform data for each car to record their movements.
+    private IList<TransformData>[] transformRecordList;
+
+
+    // Awake is called when the script instance is being loaded.
     private void Awake()
     {
-        // Set activeSelf of all cars to false except the first car.
-        for(int i=1;i<cars.Length;++i)
-            cars[i].SetActive(false);
+        // Initialize the transformRecordList with a list for each car.
+        transformRecordList=new List<TransformData>[cars.Length];
+        for(int i = 0;i<transformRecordList.Length;++i)
+            transformRecordList[i]=new List<TransformData>();
     }
 
+    // Start is called before the first frame update.
     private void Start()
     {
-        
+        // Set activeSelf of all cars to false except the first car.
+        for(int i = 1;i<cars.Length;++i)
+            cars[i].SetActive(false);
+
+        // Set activeSelf of all finishes to false except the first finish.
+        for(int i = 1;i<finishes.Length;++i)
+            finishes[i].SetActive(false);
+
+        Restart();
     }
 
-    private void Update()
+
+    /// <summary>
+    /// Turn Left Button: Rotates the current car to the left by angles degrees.
+    /// </summary>
+    public void Left()
     {
-        
+        if(currentCarIndex<cars.Length)
+            cars[currentCarIndex].transform.Rotate(angles*Vector3.forward);
+    }
+
+    /// <summary>
+    /// Turn Right Button: Rotates the current car to the right by angles degrees.
+    /// </summary>
+    public void Right()
+    {
+        if(currentCarIndex<cars.Length)
+            cars[currentCarIndex].transform.Rotate(angles*Vector3.back);
+    }
+
+    /// <summary>
+    /// Play Button: Starts recording the movements of the current car and replays the movements of previous cars.
+    /// </summary>
+    public void Play()
+    {
+        Time.timeScale=1f;
+
+        ReplayPreviousCars();
+        StartCoroutine(RecordCurrentCar());
+    }
+
+
+    /// <summary>
+    /// Next: Moves on to the next car or loads the next level if all cars have been completed.
+    /// </summary>
+    public void Next()
+    {
+        if(++currentCarIndex<cars.Length)
+        {
+            Restart();
+
+            cars[currentCarIndex].SetActive(true);
+            finishes[currentCarIndex].SetActive(true);
+        }
+        else
+        {
+            StopAllCoroutines();
+
+            int nextBuildIndex = 1+SceneManager.GetActiveScene().buildIndex;
+            if(nextBuildIndex!=SceneManager.sceneCountInBuildSettings)
+                SceneManager.LoadScene(nextBuildIndex);
+            else
+                Debug.LogWarning("Level Design Time");
+        }
+    }
+
+    /// <summary>
+    /// Restart: Restarts the current level, resetting all cars and clearing recorded movements for the current car.
+    /// </summary>
+    public void Restart()
+    {
+        Time.timeScale=0f;
+
+        StopAllCoroutines();
+        transformRecordList[currentCarIndex].Clear();
+
+        for(int i = 0;i<currentCarIndex;++i)
+            cars[i].GetComponent<Car>().ResetTransform();
+
+        playButton.SetActive(true);
+
+        turnLeftButton.SetActive(false);
+        turnRightButton.SetActive(false);
+    }
+
+
+    // Coroutine to record the movements of the current car every fixed update.
+    private IEnumerator RecordCurrentCar()
+    {
+        while(true)
+        {
+            transformRecordList[currentCarIndex].Add
+            (
+                new TransformData
+                {
+                    position=cars[currentCarIndex].transform.position,
+                    rotation=cars[currentCarIndex].transform.rotation
+                }
+            );
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    // Replays the movements of previous cars by setting their position and rotation every fixed update according to their recorded movements.
+    private void ReplayPreviousCars()
+    {
+        for(int i = 0;i<currentCarIndex;++i)
+            StartCoroutine(ReplayCar(i));
+    }
+
+    // Coroutine to replay a specific car's movements every fixed update according to its recorded movements.
+    private IEnumerator ReplayCar(int carIndex)
+    {
+        foreach(TransformData transformData in transformRecordList[carIndex])
+        {
+            cars[carIndex].transform.SetPositionAndRotation(transformData.position,transformData.rotation);
+            yield return new WaitForFixedUpdate();
+        }
     }
 }
